9b2e6b42e80a432c1223ff262dde8f8f89c748fe
Sam <ruwesam@gmail.com>
2019-11-06 12:48:46 -0500

    11/6/2019 Fixed the bug from this morning. Had to do with package names within modules. There was a bug with hibernate but it's fixed for the moment.

--- core/src/main/java/com/outfit/domain/Person.java
+++ core/src/main/java/com/outfit/domain/Person.java
@@ -5,30 +5,28 @@ import javax.persistence.*;
 @Entity
 @Table(name = "person")
 public class Person {
-
-    private int idPerson;
-    private String userName;
+    private int id;
+    private String username;
     private String password;
 
-    public Person() {
-    }
+    public Person() {}
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public int getIdPerson() {
-        return idPerson;
+    public int getId() {
+        return id;
     }
 
-    public void setIdPerson(int idPerson) {
-        this.idPerson = idPerson;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
